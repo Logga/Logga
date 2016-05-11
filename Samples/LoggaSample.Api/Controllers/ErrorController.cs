0f0140fda59a2bef3470a5fdaa7a0594760f510c
Cigano Morrison Mendez <ciganogypsy@gmail.com>
2016-05-11 03:11:29 -0300

    Fixing API sample.

---
+++ Samples/LoggaSample.Api/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace LoggaSample.Api.Controllers
+{
+    public class ErrorController : ApiController
+    {
+        // GET api/error
+        public IEnumerable<string> Get()
+        {
+            throw new Exception("This is a Web API Exception, caught by Logga via Filter Attribute. Now check http://" + Request.RequestUri.Authority + "/Logga for the list of log messages.");
+        }
+    }
+}
